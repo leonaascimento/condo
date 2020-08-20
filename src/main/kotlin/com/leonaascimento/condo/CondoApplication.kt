968aecffb6f497cb275c0eeb04932a6bf79f6a62
Leonardo Nascimento <leonardo.nascimento@gympass.com>
2020-08-20 15:49:50 -0300

    Initialize spring boot project

---
+++ src/main/kotlin/com/leonaascimento/condo/CondoApplication.kt
@@ -0,0 +1,11 @@
+package com.leonaascimento.condo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CondoApplication
+
+fun main(args: Array<String>) {
+	runApplication<CondoApplication>(*args)
+}
---
+++ src/test/kotlin/com/leonaascimento/condo/CondoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.leonaascimento.condo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CondoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
